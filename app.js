2066b43dc0d09ba4aa53b885840f4a36bd64b9d5
Roxas0428 <daichan04280507@icloud.com>
2023-11-16 16:12:28 +0900

    tab,ui完成！リファクタリングするとなぜか動かない

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
   const $tab = $doc.getElementById("js-tab");
   const $nav = $tab.querySelectorAll("[data-nav]");
   const $content = $tab.querySelectorAll("[data-content]");
+  // const ACTIVE_CLASS = "is-active";
+  const navLen = $nav.length;
   // 初期化
   const init = () => {
     $content[0].style.display = "block";
@@ -11,5 +13,29 @@
   init();
   // クリックイベント
 
-  //   console.log('$content',$content);
+  const handleClick = (e) => {
+    e.preventDefault();
+    // クリックされたnavとそのdataを取得
+
+    const $this = e.target;
+    const targetVal = $this.dataset.nav;
+    // 対象外のnav、content全てリセットする
+    let index = 0;
+    while (index < navLen) {
+      $content[index].style.display = "none";
+      $nav[index].classList.remove("is-active");
+      index++;
+    }
+    // 対象のコンテンツをアクティブ化する
+    $tab.querySelectorAll(
+      '[data-content="' + targetVal + '" ]'
+    )[0].style.display = "block";
+    $nav[targetVal].classList.add("is-active");
+  };
+  // 全nav要素に対して関数を適応
+  let index = 0;
+  while (index < navLen) {
+    $nav[index].addEventListener("click", (e) => handleClick(e));
+    index++;
+  }
 })();
